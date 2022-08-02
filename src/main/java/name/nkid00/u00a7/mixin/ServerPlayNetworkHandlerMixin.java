8a56a72b5d73db9864656a0c8214969ebb81958d
NKID00 <nkid00@pm.me>
2022-08-02 13:13:55 +0800

    feat: backport 1.17-1.19

--- src/main/java/name/nkid00/u00a7/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/name/nkid00/u00a7/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,11 +1,9 @@
 package name.nkid00.u00a7.mixin;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
+import java.util.function.Consumer;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,36 +12,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import name.nkid00.u00a7.U00A7;
 
 import net.minecraft.network.packet.c2s.play.UpdateSignC2SPacket;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.filter.FilteredMessage;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
-    @Final
-    @Shadow
-    private MinecraftServer server;
-    @Shadow
-    public ServerPlayerEntity player;
-
     @Inject(method = "onUpdateSign(Lnet/minecraft/network/packet/c2s/play/UpdateSignC2SPacket;)V", at = @At("HEAD"), cancellable = true)
     private void onUpdateSign(UpdateSignC2SPacket packet, CallbackInfo info) {
         if (U00A7.options.allowFormattingCodes && U00A7.options.allowFormattingCodesOnSigns) {
             List<String> list = List.of(packet.getText());
-            filterTexts(list).thenAcceptAsync(texts -> this.onSignUpdate(packet, (List<FilteredMessage>) texts),
-                    (Executor) this.server);
+            this.filterTexts(list, texts -> this.onSignUpdate(packet, (List<FilteredMessage<String>>)texts));
             info.cancel();
         }
     }
 
     @Shadow
-    private CompletableFuture<List<FilteredMessage>> filterTexts(List<String> texts) {
+    private void filterTexts(List<String> texts, Consumer<List<FilteredMessage<String>>> consumer) {
         throw new AssertionError();
     }
 
     @Shadow
-    private void onSignUpdate(UpdateSignC2SPacket packet, List<FilteredMessage> signText) {
+    private void onSignUpdate(UpdateSignC2SPacket packet, List<FilteredMessage<String>> signText) {
         throw new AssertionError();
     }
 }
